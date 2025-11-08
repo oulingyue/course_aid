2d3d699fe613a9ce77cec989fef8338fb25dd0fb
graceou <oulingyue@gmail.com>
2025-11-08 18:20:29 -0500

    adding code to connect to postgres db on aws rds

---
+++ backend/db_connection.py
@@ -0,0 +1,30 @@
+import psycopg2
+
+db_host = "courseaid-db.cnk8wwwum9xd.us-east-2.rds.amazonaws.com"
+db_name = "postgres"
+db_user = "courseaidadmin"
+db_pass = "courseaid5200"
+
+try: 
+    connection = psycopg2.connect(
+        host=db_host, 
+        database=db_name, 
+        user=db_user, 
+        password=db_pass,
+        port="5432"
+    )
+    cursor = connection.cursor()
+
+    #execute queries with cursor 
+    cursor.execute('SELECT version();')
+    db_version = cursor.fetchone()
+    print(db_version)
+except psycopg2.Error as e:
+    print(f"Error connecting to PostgreSQL:{e}")
+
+finally:
+    if connection:
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection closed.")
+
