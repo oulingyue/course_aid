1a8b8e11ed8e3a8673e3a5c65bcfd1b081a9a616
graceou <oulingyue@gmail.com>
2025-11-16 17:02:34 -0500

    uploading sorting function for searching by course

--- backend/sorting.sql
+++ backend/sorting.sql
@@ -1,5 +1,4 @@
 -- adding sorting procedure for search bar for documentation 
-
 CREATE OR REPLACE FUNCTION search_prof_by_name(name_input TEXT)
 RETURNS TABLE (
     first_name TEXT,
@@ -26,4 +25,34 @@ RETURNS TABLE (
 		WHERE LOWER(CONCAT(first_name, ' ', last_name)) LIKE CONCAT('%', LOWER(name_input), '%');
 	
 	END;
+$$ LANGUAGE plpgsql;
+
+-- search by course number 
+CREATE OR REPLACE FUNCTION search_prof_by_course(course_input TEXT)
+RETURNS TABLE (
+    first_name TEXT,
+    last_name TEXT,
+    avg_rating NUMERIC
+) AS $$
+BEGIN
+    RETURN QUERY
+    WITH avg_prof AS (
+        SELECT 
+            i.first_name, 
+            i.last_name, 
+            AVG(COALESCE(r.rating, 0)) AS avg_rating
+        FROM instructors i
+        LEFT JOIN review r
+            ON r.instructor_first = i.first_name
+            AND r.instructor_last = i.last_name
+        GROUP BY i.first_name, i.last_name
+    )
+    SELECT ap.first_name, ap.last_name, ap.avg_rating
+    FROM avg_prof ap
+    INNER JOIN course_section cs
+        ON ap.first_name = cs.instructor_first
+        AND ap.last_name = cs.instructor_last
+    WHERE cs.course_number = course_input
+    ORDER BY ap.avg_rating DESC;
+END;
 $$ LANGUAGE plpgsql;
